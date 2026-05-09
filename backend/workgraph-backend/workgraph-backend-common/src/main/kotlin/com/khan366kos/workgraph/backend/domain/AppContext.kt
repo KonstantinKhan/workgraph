f44fe4cc6b4fd3c289c691e058c50ddbc780d292
Константин Хан <khan@khan.local>
2026-05-09 15:18:26 +0500

    update AppContext

--- backend/workgraph-backend/workgraph-backend-common/src/main/kotlin/com/khan366kos/workgraph/backend/domain/AppContext.kt
+++ backend/workgraph-backend/workgraph-backend-common/src/main/kotlin/com/khan366kos/workgraph/backend/domain/AppContext.kt
@@ -6,6 +6,7 @@ import com.khan366kos.workgraph.backend.domain.node.NodeId
 data class AppContext(
     var requestNode: Node = Node.None,
     var responseNode: Node = Node.None,
+    var responseNodes: List<Node> = emptyList(),
 
     var nodeId: NodeId = NodeId.None,
 )
