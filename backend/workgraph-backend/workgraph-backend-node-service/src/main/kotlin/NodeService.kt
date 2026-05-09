f4f5b90a11cc70c54c9571020185ae9e2c2e6a1b
Константин Хан <khan@khan.local>
2026-05-09 14:57:39 +0500

    create

--- backend/workgraph-backend/workgraph-backend-node-service/src/main/kotlin/NodeService.kt
+++ backend/workgraph-backend/workgraph-backend-node-service/src/main/kotlin/NodeService.kt
@@ -1,50 +1,51 @@
 import com.khan366kos.workgraph.backend.domain.AppContext
+import com.khan366kos.workgraph.backend.domain.node.Node
 import com.khan366kos.workgraph.backend.domain.repository.node.DbNodeRequest
 import com.khan366kos.workgraph.backend.domain.repository.node.INodeRepository
+import com.khan366kos.workgraph.backend.domain.repository.node.NodeRepoResult
 import kotlin.uuid.ExperimentalUuidApi
 import kotlin.uuid.Uuid
 
-class NodeService(private val nodeRepository: INodeRepository) {
+class NodeService(
+    private val nodeRepo: INodeRepository
+) {
+    @OptIn(ExperimentalUuidApi::class)
+    suspend fun createNode(context: AppContext): Node =
+        when(val result = nodeRepo.create(DbNodeRequest(context.requestNode))) {
+            is NodeRepoResult.Single -> result.node
+            is NodeRepoResult.DbError -> throw result.cause
+            else -> throw RuntimeException("Unexpected result")
+        }
+
 
     @OptIn(ExperimentalUuidApi::class)
-    suspend fun createNode(context: AppContext): Unit {
+    fun readNode(context: AppContext): Unit {
 
-        with(context) {
-            nodeRepository.create(
-                DbNodeRequest(
-                    node = requestNode
-                )
-            )
-        }
-
-        @OptIn(ExperimentalUuidApi::class)
-        fun readNode(context: AppContext): Unit {
-
-            context.apply {
-
-            }
-        }
-
-        fun updateNode(context: AppContext): Unit {
-            context.apply {
-                responseNode = requestNode
-            }
-        }
-
-        fun deleteNode(context: AppContext): Unit {
-            context.apply {
-
-            }
+        context.apply {
 
         }
+    }
 
-        fun searchNode(context: AppContext) {
-            context.apply {
-
-            }
+    fun updateNode(context: AppContext): Unit {
+        context.apply {
+            responseNode = requestNode
         }
+    }
 
-        @OptIn(ExperimentalUuidApi::class)
-        private fun nodeId(): Uuid = Uuid.random()
+    fun deleteNode(context: AppContext): Unit {
+        context.apply {
+
+        }
 
     }
+
+    fun searchNode(context: AppContext) {
+        context.apply {
+
+        }
+    }
+
+    @OptIn(ExperimentalUuidApi::class)
+    private fun nodeId(): Uuid = Uuid.random()
+
+}
