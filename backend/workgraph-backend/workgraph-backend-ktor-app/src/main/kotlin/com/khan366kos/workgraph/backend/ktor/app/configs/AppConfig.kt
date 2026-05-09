a7674b31186dacac221101b5538d358d85b1e052
Константин Хан <khan@khan.local>
2026-05-09 15:40:43 +0500

    update ktor app config

--- backend/workgraph-backend/workgraph-backend-ktor-app/src/main/kotlin/com/khan366kos/workgraph/backend/ktor/app/configs/AppConfig.kt
+++ backend/workgraph-backend/workgraph-backend-ktor-app/src/main/kotlin/com/khan366kos/workgraph/backend/ktor/app/configs/AppConfig.kt
@@ -1,7 +1,9 @@
 package com.khan366kos.workgraph.backend.ktor.app.configs
 
 import NodeService
+import com.khan366kos.workgraph.backend.domain.repository.node.INodeRepository
 
 class AppConfig {
-    val nodeService: NodeService = NodeService()
+    val nodeRepository: INodeRepository = TODO()
+    val nodeService: NodeService = NodeService(nodeRepository)
 }
